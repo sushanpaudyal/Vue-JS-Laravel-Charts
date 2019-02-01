4e3bc74bc079ea6cd4d0d4c0638b214baf0fa2ce
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-02-02 02:17:54 +0545

    Creating the HTML Product Addition Form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Insert Products</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,6 +13,28 @@
                             {{ session('status') }}
                         </div>
                     @endif
+
+                        <form action="">
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input type="text" id="name" name="name" class="form-control">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="year">Year</label>
+                                <input type="number" id="year" name="year" class="form-control">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="name">Price</label>
+                                <input type="number" id="price" name="price" class="form-control">
+                            </div>
+
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-primary">Create Product</button>
+                            </div>
+                        </form>
+
 
                 </div>
             </div>
