6663251c27d9e40456982500d1a887e44bfc9d85
Sushan Paudyal <sushan.paudyal@gmail.com>
2019-02-02 21:02:04 +0545

    Creating a VUE Componnet and Adding the form

---
+++ resources/js/components/AddProductComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        <form action="">
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input type="text" id="name" name="name" class="form-control">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="year">Year</label>
+                                <input type="number" id="year" name="year" class="form-control">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="name">Price</label>
+                                <input type="number" id="price" name="price" class="form-control">
+                            </div>
+
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-primary">Create Product</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,26 +14,10 @@
                         </div>
                     @endif
 
-                        <form action="">
-                            <div class="form-group">
-                                <label for="name">Name</label>
-                                <input type="text" id="name" name="name" class="form-control">
-                            </div>
 
-                            <div class="form-group">
-                                <label for="year">Year</label>
-                                <input type="number" id="year" name="year" class="form-control">
-                            </div>
 
-                            <div class="form-group">
-                                <label for="name">Price</label>
-                                <input type="number" id="price" name="price" class="form-control">
-                            </div>
 
-                            <div class="text-center">
-                                <button type="submit" class="btn btn-primary">Create Product</button>
-                            </div>
-                        </form>
+                        <add-product-component></add-product-component>
 
 
                 </div>
